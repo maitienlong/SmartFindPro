feac1ac97f716f5d188c418d2e8130b2f86be866
quannv2506 <nguyenvanquan2506@gmail.com>
2020-10-29 12:02:46 +0700

    create layout U02.2

--- app/src/main/java/com/poly/smartfindpro/ui/user/changepass/ChangePassFragment.java
+++ app/src/main/java/com/poly/smartfindpro/ui/user/changepass/ChangePassFragment.java
@@ -9,7 +9,28 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.poly.smartfindpro.R;
+import com.poly.smartfindpro.basedatabind.BaseDataBindFragment;
+import com.poly.smartfindpro.databinding.FragmentChangePassBinding;
+import com.poly.smartfindpro.databinding.FragmentSettingUserBinding;
+import com.poly.smartfindpro.ui.user.setting.SettingUserContact;
+import com.poly.smartfindpro.ui.user.setting.SettingUserPresenter;
 
-public class ChangePassFragment extends Fragment {
+public class ChangePassFragment extends BaseDataBindFragment<FragmentChangePassBinding, ChangePassPresenter>
+        implements ChangePassContact.ViewModel{
 
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_change_pass;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
 }
