29537a6c8d2574906193a9bf058e3744c4f11036
NguyenThanhNam2710 <nguyenthanhnoem2k27@gmail.com>
2020-10-31 21:28:49 +0700

    create screen S00, S01, S02

---
+++ app/src/main/java/com/poly/smartfindpro/ui/searchProduct/SearchProductActivity.java
@@ -0,0 +1,41 @@
+package com.poly.smartfindpro.ui.searchProduct;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.poly.smartfindpro.R;
+import com.poly.smartfindpro.basedatabind.BaseDataBindActivity;
+import com.poly.smartfindpro.databinding.ActivityHomeBinding;
+import com.poly.smartfindpro.databinding.ActivitySearchProductBinding;
+import com.poly.smartfindpro.ui.home.HomeContract;
+import com.poly.smartfindpro.ui.home.HomePresenter;
+
+public class SearchProductActivity extends BaseDataBindActivity<ActivitySearchProductBinding,
+        SearchProductPresenter> implements SearchProductContract.ViewModel  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_search_product;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    public void openFragment() {
+
+    }
+}
