eac4f26a1ed5550d365b2e4b5bcb8bd07ba548c6
Nguyen Minh Hieu <hieunm@lienviettech.com.vn>
2020-11-16 13:56:12 +0700

    13:55 16/11 update code

--- app/src/main/java/com/poly/smartfindpro/ui/post/adressPost/AddressPostContract.java
+++ app/src/main/java/com/poly/smartfindpro/ui/post/adressPost/AddressPostContract.java
@@ -1,4 +1,14 @@
 package com.poly.smartfindpro.ui.post.adressPost;
 
+import com.poly.smartfindpro.basedatabind.BasePresenter;
+import com.poly.smartfindpro.basedatabind.BaseView;
+
 public interface AddressPostContract {
+    interface ViewModel extends BaseView {
+
+    }
+
+    interface Presenter extends BasePresenter {
+
+    }
 }
--- app/src/main/java/com/poly/smartfindpro/ui/post/adressPost/AddressPostFragment.java
+++ app/src/main/java/com/poly/smartfindpro/ui/post/adressPost/AddressPostFragment.java
@@ -1,12 +1,19 @@
 package com.poly.smartfindpro.ui.post.adressPost;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import com.poly.smartfindpro.R;
 import com.poly.smartfindpro.basedatabind.BaseDataBindFragment;
 import com.poly.smartfindpro.databinding.FragmentLoginBinding;
 import com.poly.smartfindpro.ui.login.loginFragment.LoginContract;
 import com.poly.smartfindpro.ui.login.loginFragment.LoginPresenter;
 
-public class AddressPostFragment extends BaseDataBindFragment<FragmentLoginBinding, LoginPresenter> implements LoginContract.ViewModel {
+public class AddressPostFragment extends BaseDataBindFragment<FragmentLoginBinding, LoginPresenter> implements AddressPostContract.ViewModel {
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_address_post;
@@ -21,4 +28,5 @@ public class AddressPostFragment extends BaseDataBindFragment<FragmentLoginBindi
     protected void initData() {
 
     }
+
 }
