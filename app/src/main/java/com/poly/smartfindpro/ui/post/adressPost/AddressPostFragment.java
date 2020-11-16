2dff0427611a4a0ff4810e59d44507b8f5557113
nguyenminhhieu <53825342+minhhieu181020@users.noreply.github.com>
2020-11-16 13:16:34 +0700

    update UI fragment_address_post.xml

--- app/src/main/java/com/poly/smartfindpro/ui/post/adressPost/AddressPostFragment.java
+++ app/src/main/java/com/poly/smartfindpro/ui/post/adressPost/AddressPostFragment.java
@@ -1,4 +1,24 @@
 package com.poly.smartfindpro.ui.post.adressPost;
 
-public class AddressPostFragment {
+import com.poly.smartfindpro.R;
+import com.poly.smartfindpro.basedatabind.BaseDataBindFragment;
+import com.poly.smartfindpro.databinding.FragmentLoginBinding;
+import com.poly.smartfindpro.ui.login.loginFragment.LoginContract;
+import com.poly.smartfindpro.ui.login.loginFragment.LoginPresenter;
+
+public class AddressPostFragment extends BaseDataBindFragment<FragmentLoginBinding, LoginPresenter> implements LoginContract.ViewModel {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_address_post;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
 }
