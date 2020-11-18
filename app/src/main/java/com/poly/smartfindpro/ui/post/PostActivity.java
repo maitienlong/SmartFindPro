33277b8d6216444a91427320445287dc4159b12d
Mai Tien Long <53802796+maitienlong@users.noreply.github.com>
2020-11-18 14:00:28 +0700

    14:00 : Update code

--- app/src/main/java/com/poly/smartfindpro/ui/post/PostActivity.java
+++ app/src/main/java/com/poly/smartfindpro/ui/post/PostActivity.java
@@ -4,20 +4,21 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.poly.smartfindpro.R;
 import com.poly.smartfindpro.basedatabind.BaseDataBindActivity;
 import com.poly.smartfindpro.databinding.ActivityPostBinding;
 import com.poly.smartfindpro.ui.post.adapter.ViewPagerPostAdapter;
 import com.poly.smartfindpro.ui.post.adressPost.AddressPostFragment;
 
-public class PostActivity extends BaseDataBindActivity<ActivityPostBinding, PostPresenter> implements PostContract.ViewModel{
+public class PostActivity extends BaseDataBindActivity<ActivityPostBinding, PostPresenter> implements PostContract.ViewModel {
 
     private boolean isStatusInfor = false, isStatusAddress = false, isStatusTools = false, isStatusConfirm = false;
-
-    private ViewPager viewPager;
 
     private ViewPagerPostAdapter viewPagerPostAdapter;
 
@@ -28,17 +29,37 @@ public class PostActivity extends BaseDataBindActivity<ActivityPostBinding, Post
 
     @Override
     protected void initView() {
-        viewPager = mBinding.vpPost;
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        viewPagerPostAdapter = new ViewPagerPostAdapter(this);
+        mBinding.vpPost.setAdapter(viewPagerPostAdapter);
 
-        viewPagerPostAdapter = new ViewPagerPostAdapter(fragmentManager);
+        mBinding.vpPost.setCurrentItem(1);
 
-        viewPager.setAdapter(viewPagerPostAdapter);
+//        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(mBinding.tlPost, mBinding.vpPost, new TabLayoutMediator.TabConfigurationStrategy() {
+//            @Override
+//            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+//                switch (position){
+//                    case 0:
+//                        tab.setIcon(R.mipmap.btn_rdo_false);
+//                        tab.setText("Thông tin");
+//                        break;
+//                    case 1:
+//                        tab.setIcon(R.mipmap.btn_rdo_false);
+//                        tab.setText("Địa chỉ");
+//                        break;
+//                    case 3:
+//                        tab.setIcon(R.mipmap.btn_rdo_false);
+//                        tab.setText("Tiện ích");
+//                        break;
+//                    case 4:
+//                        tab.setIcon(R.mipmap.btn_rdo_false);
+//                        tab.setText("Xác nhận ");
+//                        break;
+//                }
+//            }
+//        });
+//        tabLayoutMediator.attach();
 
-        viewPager.setCurrentItem(0);
-
-        //viewPager.addOnPageChangeListener();
     }
 
     @Override
@@ -47,26 +68,26 @@ public class PostActivity extends BaseDataBindActivity<ActivityPostBinding, Post
 
     }
 
-    @Override
-    public void onClick(View v) {
-        super.onClick(v);
-        switch (v.getId()) {
-            case R.id.btn_infor:
-
-                Toast.makeText(PostActivity.this, "A", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.btn_address:
-                Toast.makeText(PostActivity.this, "A", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.btn_tool:
-                Toast.makeText(PostActivity.this, "A", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.btn_confirm:
-                Toast.makeText(PostActivity.this, "A", Toast.LENGTH_SHORT).show();
-                break;
-        }
-    }
+//    @Override
+//    public void onClick(View v) {
+//        super.onClick(v);
+//        switch (v.getId()) {
+//            case R.id.btn_infor:
+//
+//                Toast.makeText(PostActivity.this, "A", Toast.LENGTH_SHORT).show();
+//                break;
+//
+//            case R.id.btn_address:
+//                Toast.makeText(PostActivity.this, "A", Toast.LENGTH_SHORT).show();
+//                break;
+//
+//            case R.id.btn_tool:
+//                Toast.makeText(PostActivity.this, "A", Toast.LENGTH_SHORT).show();
+//                break;
+//
+//            case R.id.btn_confirm:
+//                Toast.makeText(PostActivity.this, "A", Toast.LENGTH_SHORT).show();
+//                break;
+//        }
+//    }
 }
--- app/src/main/java/com/poly/smartfindpro/ui/post/adapter/ViewPagerPostAdapter.java
+++ app/src/main/java/com/poly/smartfindpro/ui/post/adapter/ViewPagerPostAdapter.java
@@ -2,34 +2,48 @@ package com.poly.smartfindpro.ui.post.adapter;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-public class ViewPagerPostAdapter extends FragmentStatePagerAdapter {
-    public ViewPagerPostAdapter(@NonNull FragmentManager fm) {
-        super(fm);
+import com.poly.smartfindpro.ui.post.adressPost.AddressPostFragment;
+import com.poly.smartfindpro.ui.post.inforPost.InforPostFragment;
+
+public class ViewPagerPostAdapter extends FragmentStateAdapter {
+
+    public ViewPagerPostAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
     }
 
     @NonNull
     @Override
-    public Fragment getItem(int position) {
+    public Fragment createFragment(int position) {
         Fragment fragment = null;
-        switch (position){
-            case 0:
 
+        switch (position) {
+            case 0:
+                fragment = new AddressPostFragment();
                 break;
             case 1:
+                fragment = new InforPostFragment();
+
+
                 break;
             case 2:
+
                 break;
             case 3:
-                break;
+
+                break ;
+            default:
+
         }
         return fragment;
     }
 
     @Override
-    public int getCount() {
+    public int getItemCount() {
         return 4;
     }
 }
