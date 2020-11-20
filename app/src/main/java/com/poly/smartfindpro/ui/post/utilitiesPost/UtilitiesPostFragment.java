bbcdf2f8860be0b4210ec3ecbec0c0b582ca78d2
nguyenminhhieu <53825342+minhhieu181020@users.noreply.github.com>
2020-11-20 09:48:23 +0700

    10:16 18/11 Update Utilities

--- app/src/main/java/com/poly/smartfindpro/ui/post/utilitiesPost/UtilitiesPostFragment.java
+++ app/src/main/java/com/poly/smartfindpro/ui/post/utilitiesPost/UtilitiesPostFragment.java
@@ -2,6 +2,7 @@ package com.poly.smartfindpro.ui.post.utilitiesPost;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -10,27 +11,40 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView.Adapter;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.poly.smartfindpro.R;
 import com.poly.smartfindpro.basedatabind.BaseDataBindFragment;
+import com.poly.smartfindpro.data.Config;
 import com.poly.smartfindpro.databinding.FragmentAddressPostBinding;
 import com.poly.smartfindpro.databinding.FragmentUtilitiesPostBinding;
 import com.poly.smartfindpro.ui.post.adapter.UtilitiesAdapter;
 import com.poly.smartfindpro.ui.post.adressPost.AddressPostContract;
 import com.poly.smartfindpro.ui.post.adressPost.AddressPostPresenter;
+import com.poly.smartfindpro.ui.post.model.PostRequest;
 import com.poly.smartfindpro.ui.post.utilitiesPost.model.UtilitiesModel;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
-public class UtilitiesPostFragment extends BaseDataBindFragment<FragmentUtilitiesPostBinding, UtilitiesPresenter> implements UtilitiesContract.ViewModel, View.OnClickListener {
+public class UtilitiesPostFragment extends BaseDataBindFragment<FragmentUtilitiesPostBinding, UtilitiesPresenter> implements UtilitiesContract.ViewModel {
+private PostRequest postRequest;
 
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_utilities_post;
     }
+    private void ReciData() {
+        Type type = new TypeToken<PostRequest>() {
+        }.getType();
 
+        postRequest = new Gson().fromJson(getArguments().getString(Config.POST_BUNDEL_RES), type);
+
+    }
     @Override
     protected void initView() {
-
+        ReciData();
 
     }
 
@@ -38,18 +52,25 @@ public class UtilitiesPostFragment extends BaseDataBindFragment<FragmentUtilitie
     protected void initData() {
         mPresenter = new UtilitiesPresenter(mActivity, this);
         mBinding.setPresenter(mPresenter);
+mPresenter.setPostRequest(postRequest);
 
         mPresenter.CreateData();
-        UtilitiesAdapter utilitiesAdapter = new UtilitiesAdapter(mActivity);
+        UtilitiesAdapter utilitiesAdapter = new UtilitiesAdapter(mActivity, this);
         utilitiesAdapter.setListItem(mPresenter.CreateData());
 
-        GridLayoutManager linearLayoutManager = new GridLayoutManager(mActivity,2);
+        GridLayoutManager linearLayoutManager = new GridLayoutManager(mActivity, 2);
         mBinding.rcUtilities.setAdapter(utilitiesAdapter);
         mBinding.rcUtilities.setLayoutManager(linearLayoutManager);
     }
 
-    @Override
-    public void onClick(View view) {
 
+    @Override
+    public void onBackData(List<UtilitiesModel> arrayList) {
+        mPresenter.setmListUpdate(arrayList);
+    }
+
+    @Override
+    public void onNext(String jsonData) {
+        Log.d("CheckLog", jsonData  );
     }
 }
