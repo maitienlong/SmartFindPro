a71c909cadfc967cbf92bd3c612f00795e4dab29
Mai Tien Long <53802796+maitienlong@users.noreply.github.com>
2020-11-26 17:11:11 +0700

    update: done get area

--- app/src/main/java/com/poly/smartfindpro/ui/post/adressPost/AddressPostContract.java
+++ app/src/main/java/com/poly/smartfindpro/ui/post/adressPost/AddressPostContract.java
@@ -6,14 +6,20 @@ import com.poly.smartfindpro.data.model.area.result.ResultArea;
 
 public interface AddressPostContract {
     interface ViewModel extends BaseView {
+        void onShowProvince(ResultArea resultArea);
+
         void onShowDistrict(ResultArea resultArea);
 
         void onShowCommune(ResultArea resultArea);
+
+        void onSubmitData();
     }
 
     interface Presenter extends BasePresenter {
 
         void getDataApiArea(int areaType, String jsonData);
+
+        void onNext();
 
     }
 }
