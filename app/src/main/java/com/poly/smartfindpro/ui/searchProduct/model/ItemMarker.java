e2f315ad84a6b7653d6a31c355ce438cac782140
Thái <thaiphph07639@fpt.edu.vn>
2020-12-08 00:55:44 +0700

    .

--- app/src/main/java/com/poly/smartfindpro/ui/searchProduct/model/ItemMarker.java
+++ app/src/main/java/com/poly/smartfindpro/ui/searchProduct/model/ItemMarker.java
@@ -1,47 +1,52 @@
 package com.poly.smartfindpro.ui.searchProduct.model;
 
-class ItemMarker {
-    Double lat;
-    Double log;
-    String name;
-    String address;
+public class ItemMarker {
+    package com.poly.smartfindpro.ui.searchProduct.model;
 
-    public ItemMarker(Double lat, Double log, String name, String address) {
-        this.lat = lat;
-        this.log = log;
-        this.name = name;
-        this.address = address;
+    class ItemMarker {
+        Double lat;
+        Double log;
+        String name;
+        String address;
+
+        public ItemMarker(Double lat, Double log, String name, String address) {
+            this.lat = lat;
+            this.log = log;
+            this.name = name;
+            this.address = address;
+        }
+
+        public Double getLat() {
+            return lat;
+        }
+
+        public void setLat(Double lat) {
+            this.lat = lat;
+        }
+
+        public Double getLog() {
+            return log;
+        }
+
+        public void setLog(Double log) {
+            this.log = log;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
     }
 
-    public Double getLat() {
-        return lat;
-    }
-
-    public void setLat(Double lat) {
-        this.lat = lat;
-    }
-
-    public Double getLog() {
-        return log;
-    }
-
-    public void setLog(Double log) {
-        this.log = log;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
