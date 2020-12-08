9b94c937c48d367dc4df415a1244c75ec8142cf8
Thái <thaiphph07639@fpt.edu.vn>
2020-12-09 00:18:31 +0700

    send otp token

--- app/src/main/java/com/poly/smartfindpro/ui/login/otp/ConfirmOTPFragment.java
+++ app/src/main/java/com/poly/smartfindpro/ui/login/otp/ConfirmOTPFragment.java
@@ -2,13 +2,22 @@ package com.poly.smartfindpro.ui.login.otp;
 
 import android.view.View;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.poly.smartfindpro.R;
 import com.poly.smartfindpro.basedatabind.BaseDataBindFragment;
 import com.poly.smartfindpro.databinding.FragmentConfirmOtpBinding;
 import com.poly.smartfindpro.databinding.FragmentLoginBinding;
 import com.poly.smartfindpro.ui.login.createPassword.CreatePasswordFragment;
 
+import java.util.concurrent.TimeUnit;
+
 public class ConfirmOTPFragment extends BaseDataBindFragment<FragmentConfirmOtpBinding, ConfirmOTPPresenter> implements ConfirmOTPContract.ViewModel {
+    private String verificationId;
+
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_confirm_otp;
@@ -20,10 +29,11 @@ public class ConfirmOTPFragment extends BaseDataBindFragment<FragmentConfirmOtpB
         mBinding.btnAction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getBaseActivity().goToFragmentReplace(R.id.fl_Login,new CreatePasswordFragment(), null);
-            }
-        });
+                getBaseActivity().goToFragmentReplace(R.id.fl_Login, new CreatePasswordFragment(), null);
 
+            }
+
+        });
     }
 
     @Override
--- app/src/main/java/com/poly/smartfindpro/ui/login/registerFragment/RegisterFragment.java
+++ app/src/main/java/com/poly/smartfindpro/ui/login/registerFragment/RegisterFragment.java
@@ -1,14 +1,29 @@
 package com.poly.smartfindpro.ui.login.registerFragment;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.FirebaseException;
+//import com.google.firebase.auth.PhoneAuthCredential;
+//import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.poly.smartfindpro.R;
 import com.poly.smartfindpro.basedatabind.BaseDataBindFragment;
 import com.poly.smartfindpro.databinding.FragmentLoginBinding;
 import com.poly.smartfindpro.databinding.FragmentRegisterBinding;
 import com.poly.smartfindpro.ui.login.otp.ConfirmOTPFragment;
 
+import java.util.concurrent.TimeUnit;
+
 public class RegisterFragment extends BaseDataBindFragment<FragmentRegisterBinding, RegisterPresenter> implements RegisterContract.ViewModel {
+
+    private static String code = "";
+
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_register;
@@ -28,7 +43,32 @@ public class RegisterFragment extends BaseDataBindFragment<FragmentRegisterBindi
 
     @Override
     protected void initData() {
+        mBinding.btnAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                        mBinding.edtAccountNumber.getText().toString(), 60, TimeUnit.SECONDS, getActivity(),
+                        new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                            @Override
+                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
 
+                            }
+
+                            @Override
+                            public void onVerificationFailed(@NonNull FirebaseException e) {
+                                Toast.makeText(getContext(), "chưa có số điện thoại", Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                super.onCodeSent(s, forceResendingToken);
+                                code = s;
+                            }
+                        }
+                );
+
+            }
+        });
     }
 
 
