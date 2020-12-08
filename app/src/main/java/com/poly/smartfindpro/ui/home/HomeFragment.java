0975e2e54ea6a4dffb78fdf2587dacd3f86e7173
quannv2506 <nguyenvanquan2506@gmail.com>
2020-12-08 23:13:12 +0700

    quan update UI

--- app/src/main/java/com/poly/smartfindpro/ui/home/HomeFragment.java
+++ app/src/main/java/com/poly/smartfindpro/ui/home/HomeFragment.java
@@ -1,8 +1,11 @@
 package com.poly.smartfindpro.ui.home;
 
+import android.annotation.SuppressLint;
+
 import com.poly.smartfindpro.R;
 import com.poly.smartfindpro.basedatabind.BaseDataBindFragment;
 import com.poly.smartfindpro.data.model.home.res.Product;
+import com.poly.smartfindpro.data.model.product.res.Products;
 import com.poly.smartfindpro.databinding.FragmentHomeBinding;
 import com.poly.smartfindpro.ui.home.adapter.HomeAdapter;
 import com.poly.smartfindpro.ui.post.PostActivity;
@@ -17,7 +20,7 @@ import ss.com.bannerslider.Slider;
 
 public class HomeFragment extends BaseDataBindFragment<FragmentHomeBinding, HomePresenter> implements HomeContract.ViewModel {
     private HomeAdapter homeAdapter;
-    private ArrayList<Product> product = new ArrayList<>();
+//    private ArrayList<Product> product = new ArrayList<>();
 
     private List<String> mListImage;
 
@@ -56,14 +59,32 @@ public class HomeFragment extends BaseDataBindFragment<FragmentHomeBinding, Home
 
     }
 
+    @Override
     public void onShow(List<Product> productList) {
         homeAdapter.setListItem(productList);
         BindingUtils.setAdapter(mBinding.rvList, homeAdapter, true);
     }
 
+
     @Override
     public void openPost() {
         getBaseActivity().openActivity(PostActivity.class);
+    }
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    public void onClickRentalRoom() {
+//        mBinding.btnRentalRoom.setBackgroundColor(R.color.blue);
+//        mBinding.btnRentalRoom.setCardBackgroundColor(R.color.blue);
+//        mBinding.btnRentalRoom.setRadius(R.color.blue);
+////        mBinding.tvRentalRoom.
+//        mBinding.btnShareRoom.setBackgroundColor(R.color.white);
+    }
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    public void onClickShareRoom() {
+//
     }
 
 }
--- app/src/main/java/com/poly/smartfindpro/ui/home/HomePresenter.java
+++ app/src/main/java/com/poly/smartfindpro/ui/home/HomePresenter.java
@@ -1,11 +1,20 @@
 package com.poly.smartfindpro.ui.home;
 
 import android.content.Context;
+import android.util.Log;
 
+import com.google.gson.Gson;
 import com.poly.smartfindpro.data.model.home.req.HomeRequest;
 import com.poly.smartfindpro.data.model.home.res.HomeResponse;
+import com.poly.smartfindpro.data.model.home.res.Product;
+import com.poly.smartfindpro.data.model.product.req.ProductRequest;
+import com.poly.smartfindpro.data.model.product.res.ProductResponse;
+import com.poly.smartfindpro.data.model.product.res.Products;
 import com.poly.smartfindpro.data.retrofit.MyRetrofitSmartFind;
 import com.poly.smartfindpro.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -15,6 +24,7 @@ public class HomePresenter implements HomeContract.Presenter {
     private Context mContext;
     private HomeContract.ViewModel mViewmodel;
     private FragmentHomeBinding mBinding;
+    private List<Product> productsList;
 
     public HomePresenter(Context mContext, HomeContract.ViewModel mViewmodel, FragmentHomeBinding mBinding) {
         this.mContext = mContext;
@@ -24,11 +34,25 @@ public class HomePresenter implements HomeContract.Presenter {
     }
     private void initData() {
 
-        getProduct();
+//        getProduct();
+        getProductRental();
     }
+
     @Override
     public void openPost() {
         mViewmodel.openPost();
+    }
+
+    @Override
+    public void onClickRentalRoom() {
+        mViewmodel.onClickRentalRoom();
+        getProductRental();
+    }
+
+    @Override
+    public void onClickShareRoom() {
+        mViewmodel.onClickShareRoom();
+        getProductShare();
     }
 
     @Override
@@ -40,7 +64,8 @@ public class HomePresenter implements HomeContract.Presenter {
     public void unSubscribe() {
 
     }
-    private void getProduct() {
+
+    private void getProductRental(){
         HomeRequest request = new HomeRequest();
         request.setId("5fb2073ff69b03b8f8875059");
 
@@ -48,7 +73,16 @@ public class HomePresenter implements HomeContract.Presenter {
             @Override
             public void onResponse(Call<HomeResponse> call, Response<HomeResponse> response) {
                 if (response.code() == 200) {
-                    mViewmodel.onShow(response.body().getResponseBody().getProducts());
+                    productsList = new ArrayList<>();
+                    for (int i=0; i< response.body().getResponseBody().getProducts().size(); i++){
+                        if (response.body().getResponseBody().getProducts().get(i).getProduct().getCategory().equals("Nhà trọ")
+                                || response.body().getResponseBody().getProducts().get(i).getProduct().getCategory().equals("Chung cư")
+                                || response.body().getResponseBody().getProducts().get(i).getProduct().getCategory().equals("Nguyên căn")){
+                            productsList.add(response.body().getResponseBody().getProducts().get(i));
+                        }
+                    }
+                    Log.d("list111", "onResponse: " + productsList.size());
+                    mViewmodel.onShow(productsList);
 
                 } else {
 
@@ -61,4 +95,58 @@ public class HomePresenter implements HomeContract.Presenter {
             }
         });
     }
+
+    private void getProductShare(){
+        HomeRequest request = new HomeRequest();
+        request.setId("5fb2073ff69b03b8f8875059");
+
+        MyRetrofitSmartFind.getInstanceSmartFind().getProduct(request).enqueue(new Callback<HomeResponse>() {
+            @Override
+            public void onResponse(Call<HomeResponse> call, Response<HomeResponse> response) {
+                if (response.code() == 200) {
+                    productsList = new ArrayList<>();
+                    for (int i=0; i< response.body().getResponseBody().getProducts().size(); i++){
+                        if (response.body().getResponseBody().getProducts().get(i).getProduct().getCategory().equals("Ở ghép")){
+                            productsList.add(response.body().getResponseBody().getProducts().get(i));
+                        }
+                    }
+                    Log.d("list111", "onResponse: " + productsList.size());
+                    mViewmodel.onShow(productsList);
+
+                } else {
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<HomeResponse> call, Throwable t) {
+
+            }
+        });
+    }
+
+
+//    private void getProduct() {
+//        ProductRequest request = new ProductRequest();
+//        request.setId("5fb2073ff69b03b8f8875059");
+//
+//        MyRetrofitSmartFind.getInstanceSmartFind().getProduct(request).enqueue(new Callback<ProductResponse>() {
+//            @Override
+//            public void onResponse(Call<ProductResponse> call, Response<ProductResponse> response) {
+//                if (response.code() == 200) {
+////                    if (response.body().getResponseBody().getProducts())
+//                    mViewmodel.onShow(response.body().getResponseBody().getProducts());
+//
+//
+//                } else {
+//
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<ProductResponse> call, Throwable t) {
+//
+//            }
+//        });
+//    }
 }
