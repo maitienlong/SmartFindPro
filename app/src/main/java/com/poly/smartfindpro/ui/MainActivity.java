ce904e6d5533da1939cdfd2a64138d47a1369856
nguyenminhhieu <53825342+minhhieu181020@users.noreply.github.com>
2020-12-24 03:03:47 +0700

    3:03 24/12 update

--- app/src/main/java/com/poly/smartfindpro/ui/MainActivity.java
+++ app/src/main/java/com/poly/smartfindpro/ui/MainActivity.java
@@ -71,6 +71,7 @@ public class MainActivity extends BaseDataBindActivity<ActivityMainBinding,
                 mBinding.btnHome.setImageResource(R.drawable.ic_outline_home);
                 mBinding.btnMessage.setImageResource(R.drawable.ic_outline_message);
                 mBinding.btnUser.setImageResource(R.drawable.ic_person_full);
+                goToFragmentReplaceLeft(R.id.fl_native,new UserFragment(),null);
                 break;
         }
     }
